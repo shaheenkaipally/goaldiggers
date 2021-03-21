508b2e6a94d246366b6227219e85e077d833dcb9
Keval <keval7126@gmail.com>
2021-03-21 14:38:42 +0530

    make it better

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+module.exports = {
+    "URI": "mongodb+srv://shaheen:shaheen@cluster0.oogdf.mongodb.net/tournament?retryWrites=true&w=majority"
+
+
+}
---
+++ server/routes/index.js
@@ -0,0 +1,13 @@
+let express = require('express');
+let router = express.Router();
+
+let indexController = require('../controllers/index');
+
+/* GET home page. */
+router.get('/', indexController.displayHomePage);
+
+/* GET home page. */
+router.get('/home', indexController.displayHomePage);
+
+
+module.exports = router;
